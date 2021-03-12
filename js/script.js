945c4e0967b75a855ff08c9f573d15c61545d21a
Aleksandr <ledich19@gmail.com>
2021-03-12 17:24:05 +0200

    comment

---
+++ js/script.js
@@ -0,0 +1,42 @@
+"use strict";
+//alert("hello");
+// const result = confirm("Are uoy here?");
+// console.log(result);
+
+// const answer = +prompt("ваш возрвст 18?", "18");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt("Как ваше имя", "");
+// answers[1] = prompt("Как ваша фамилия", "");
+// answers[2] = prompt("Какой ваш возраст", "");
+
+// document.write(answers);
+// console.log(typeof(answers));
+
+
+
+// const category = 'toys';
+// console.log(`htttps://someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+// alert(`hello, ${user}`);
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(incr++);
+console.log(decr--);
+console.log(5%2);
+console.log(2*4 == 8);
+console.log(2*4 == "8");
+console.log(2*4 === "8");
+
+const isChecked = true,
+      isClose = false;
+      console.log(isChecked && isClose);
+      console.log(isChecked || isClose);
