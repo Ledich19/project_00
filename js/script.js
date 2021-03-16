13635694f1a3e5ca314b6e45ee38331e1b4c1cd0
Aleksandr <ledich19@gmail.com>
2021-03-16 15:46:39 +0200

    mine code :) terrible

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 /* Задания на урок:
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
 
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
 
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
 
-5) Добавить нумерацию выведенных фильмов */
+5) Фильмы должны быть отсортированы по алфавиту */
 
 'use strict';
 
@@ -49,7 +52,58 @@ const filmitems = movielist.querySelectorAll(".promo__interactive-item");
 movielist.innerHTML = "";
 movieDB.movies.sort();
 movieDB.movies.forEach((item, i) => {
-        movielist.innerHTML += `<li class="promo__interactive-item">${i + 1} - ${item}
+    movielist.innerHTML += `<li class="promo__interactive-item">${i + 1} - ${item}
         <div class="delete"></div>
     </li>`;
+});
+
+const input = document.querySelector('.add input');
+const button = document.querySelector('.add button');
+
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (input.value.length > 21) {
+        input.value = input.value.slice(0, 21) + '...';
+        movieDB.movies.push(input.value);
+    } else {
+        movieDB.movies.push(input.value);
+    }
+
+    if (document.querySelector('input[type="checkbox" i]').checked == true) {
+        console.log('Добавляем любимый фильм');
+    } else {
+        console.log(false);
+    }
+
+    greateMovieList ();
+});
+console.log(document.querySelector('input[type="checkbox" i]'));
+
+
+
+function deleteFilm() {
+    const deleteFilmButton = document.querySelectorAll('.delete');
+    deleteFilmButton.forEach((element, i) => {
+        element.addEventListener('click', (e) => {
+            delete movieDB.movies[i];
+            console.log(movieDB.movies);
+            greateMovieList ();
+            deleteFilm();
+        });
     });
+}
+deleteFilm();
+
+function greateMovieList () {
+    movielist.innerHTML = "";
+            movieDB.movies.sort();
+            movieDB.movies.forEach((item, i) => {
+                movielist.innerHTML += `<li class="promo__interactive-item">${i + 1} - ${item}
+                <div class="delete"></div>
+            </li>`;
+            });
+}
+
+
+console.log(document.querySelector('input[type="checkbox" i]'));
